71aa9f4cdf00f8f4ea7b67fc136514b1b15dabdd
Jeff Sutherland <jeffdsuth@gmail.com>
2020-06-28 13:32:13 -0700

    Switch to conditional ex

--- src/index.js
+++ src/index.js
@@ -2,39 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({date: new Date()})
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    )
-  }
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
 }
 
-function tick() {
-  ReactDOM.render(
-    <Clock />,
-    document.getElementById('root')
-  );
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
 }
 
-setInterval(tick, 1000);
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+ReactDOM.render(
+  // Try changing to isLoggedIn={true}:
+  <Greeting isLoggedIn={false} />,
+  document.getElementById('root')
+);
